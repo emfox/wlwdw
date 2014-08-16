944e3157c88d6d0f11bf14b8c6ff4be8d3dee82f
Emfox Zhou <EmfoxZhou@gmail.com>
2014-08-16 17:26:47 +0800

    Proxy all baidumap api request so that browser won't complain for non-secure of mixing https and http contents.

--- src/Emfox/GpsBundle/Controller/DefaultController.php
+++ src/Emfox/GpsBundle/Controller/DefaultController.php
@@ -3,6 +3,8 @@
 namespace Emfox\GpsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -10,4 +12,22 @@ class DefaultController extends Controller
     {
         return $this->render('EmfoxGpsBundle:Default:index.html.twig');
     }
+
+	public function proxyAction(Request $request, $url)
+	{
+		$content = file_get_contents($url . '?' . $request->getQueryString());
+		$response = new Response();
+		foreach ($http_response_header as $value)
+		{
+			if (preg_match('/^Content-Type:(.*)/i', $value, $matches)) {
+				$type = $matches[1];
+				$response->headers->set('Content-Type', $type);
+				if (preg_match('/javascript/i', $type)) {
+					$content = preg_replace('@http://@', '/proxy/http://', $content);
+				}
+			}
+		}
+		$response->setContent($content);
+		return $response;
+	}
 }
