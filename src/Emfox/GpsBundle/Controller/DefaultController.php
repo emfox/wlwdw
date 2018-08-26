285da95c125b3a44980133e76b815ac5a459373a
Emfox Zhou <EmfoxZhou@gmail.com>
2018-08-26 07:17:25 -0400

    BaiduMap js sdk support https now(though a little tricky), so reverse proxy removed.

--- src/Emfox/GpsBundle/Controller/DefaultController.php
+++ src/Emfox/GpsBundle/Controller/DefaultController.php
@@ -3,8 +3,6 @@
 namespace Emfox\GpsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -12,22 +10,4 @@ class DefaultController extends Controller
     {
         return $this->render('EmfoxGpsBundle:Default:index.html.twig',array('maptype' => $maptype));
     }
-
-	public function proxyAction(Request $request, $url)
-	{
-		$content = file_get_contents($url . '?' . $request->getQueryString());
-		$response = new Response();
-		foreach ($http_response_header as $value)
-		{
-			if (preg_match('/^Content-Type:(.*)/i', $value, $matches)) {
-				$type = $matches[1];
-				$response->headers->set('Content-Type', $type);
-				if (preg_match('/javascript/i', $type)) {
-					$content = preg_replace('@(http://)(?!www\.w3\.org)@', '/proxy/$1', $content);
-				}
-			}
-		}
-		$response->setContent($content);
-		return $response;
-	}
 }
