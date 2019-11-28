11c5134e0c288842778fd5eb8448c7560050740a
Emfox Zhou <emfoxzhou@gmail.com>
2019-11-28 13:30:54 +0800

    Remove some deprecations of Symfony 4.4

--- config/bundles.php
+++ config/bundles.php
@@ -12,7 +12,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
