f763f21dbc58b1d7a1f377fb8c49c93929275dbe
Emfox Zhou <emfoxzhou@gmail.com>
2025-04-16 20:30:20 +0800

    Rewrite logic to use new Baidu Location API, import service.locService instead of PollingService.

--- android/app/src/main/java/com/wlwdw/gps1s/LocationActivity.java
+++ android/app/src/main/java/com/wlwdw/gps1s/LocationActivity.java
@@ -7,18 +7,25 @@ import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
 
 import com.baidu.mapapi.model.LatLng;
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.Poi;
+import com.baidu.location.PoiRegion;
+import com.wlwdw.gps1s.service.LocService;
 
-import android.content.BroadcastReceiver;
+import android.app.Notification;
+import android.app.PendingIntent;
+
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.content.pm.PackageManager;
+
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.appcompat.app.AppCompatActivity;
-import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -47,18 +54,21 @@ public class LocationActivity extends AppCompatActivity {
 	private RadioGroup selectMode,selectCoordinates;
 	//默认2分钟定位间隔。 注意，该间隔与百度API的定位间隔span无关。
 	//采用 alarmManager 实现轮询，更好支持系统休眠时也能唤醒并定位，故不使用API提供的定时功能
+	private NotificationUtils mNotificationUtils;
+	private Notification notification;
 	private String errString;
 	private Integer interval=2; 
 	private Integer tempMode = 1;
 	private String tempcoor="bd09ll";
 	private CheckBox checkGeoLocation;
 
-	private LocalBroadcastManager mLocalBroadcastManager;
-	private BroadcastReceiver mReceiver;
+	private LocService locService;
+	private boolean isLocPolling;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
 		sharedEditor = sharedPref.edit();
 		if(sharedPref.getString("app_uuid", "").isEmpty()) {
@@ -66,7 +76,6 @@ public class LocationActivity extends AppCompatActivity {
 		}
 		startBlackService();
 		setContentView(R.layout.location);
-		mLocalBroadcastManager = LocalBroadcastManager.getInstance(this);
 		LabelTime = (TextView)findViewById(R.id.LabelTime);
 		LabelErrcode = (TextView)findViewById(R.id.LabelErrcode);
 		LabelLatLng = (TextView)findViewById(R.id.LabelLatLng);
@@ -78,31 +87,51 @@ public class LocationActivity extends AppCompatActivity {
 		 checkGeoLocation = (CheckBox)findViewById(R.id.geolocation);
 		startLocation = (Button)findViewById(R.id.addfence);
 		onceLocate = (Button)findViewById(R.id.oncelocate);
-		if(PollingService.isPolling)
-				startLocation.setText(getString(R.string.stoplocation));
+
+		//设置后台定位
+		//android8.0及以上使用NotificationUtils
+		if (Build.VERSION.SDK_INT >= 26) {
+			mNotificationUtils = new NotificationUtils(this);
+			Notification.Builder builder2 = mNotificationUtils.getAndroidChannelNotification
+					("适配android 8限制后台定位功能", "正在后台定位");
+			notification = builder2.build();
+		} else {
+			//获取一个Notification构造器
+			Notification.Builder builder = new Notification.Builder(LocationActivity.this);
+			Intent nfIntent = new Intent(LocationActivity.this, LocationActivity.class);
+
+			builder.setContentIntent(PendingIntent.
+							getActivity(LocationActivity.this, 0, nfIntent, 0)) // 设置PendingIntent
+					.setContentTitle("适配android 8限制后台定位功能") // 设置下拉列表里的标题
+					.setSmallIcon(R.drawable.ic_launcher) // 设置状态栏内的小图标
+					.setContentText("正在后台定位") // 设置上下文内容
+					.setWhen(System.currentTimeMillis()); // 设置该通知发生的时间
+
+			notification = builder.build(); // 获取构建好的Notification
+		}
+		notification.defaults = Notification.DEFAULT_SOUND; //设置为默认的声音
+
+		locService = ((LocationApplication) getApplication()).locService;
+		boolean isRegSuccess = locService.registerListener(mListener);
+
+		checkGeoLocation.setChecked(sharedPref.getBoolean("NeedAddr", false));
 		startLocation.setOnClickListener(new OnClickListener() {
-			
 			@Override
 			public void onClick(View v) {
-				InitLocation();
-				
-				if(!PollingService.isPolling){
-
-					System.out.println("Start polling service...");  
-			        PollingUtils.startPollingService(LocationActivity.this, interval*60, PollingService.class);
-			        PollingService.isPolling = true;
-			        startLocation.setText(getString(R.string.stoplocation));
-				}else{
-
-					System.out.println("Stop polling service...");  
-			        PollingUtils.stopPollingService(LocationActivity.this, PollingService.class);
-			        PollingService.isPolling = false;
+				if (!isLocPolling) {
+					locService.getClient().enableLocInForeground(1, notification);
+					locService.start();
+					startLocation.setText(getString(R.string.stoplocation));
+					isLocPolling = true;
+				} else {
+					locService.getClient().disableLocInForeground(true);
+					locService.stop();
 					startLocation.setText(getString(R.string.startlocation));
+					isLocPolling = false;
 				}
-				
-				
 			}
 		});
+
 		onceLocate.setOnClickListener(new OnClickListener() {
 			
 			@Override
@@ -163,46 +192,22 @@ public class LocationActivity extends AppCompatActivity {
 			}
 		});
 	}
+
+	@Override
+	protected void onDestroy(){
+        super.onDestroy();
+		locService.unregisterListener(mListener);
+		if(isLocPolling) {
+			locService.getClient().disableLocInForeground(true);
+			locService.stop();
+		}
+
+
+    }
 	@Override
 	protected void onStart() {
 		super.onStart();
-		checkGeoLocation.setChecked(sharedPref.getBoolean("NeedAddr", false));
-		IntentFilter filter = new IntentFilter();    
-		filter.addAction("LocationResult");
-		mReceiver = new BroadcastReceiver() {    
-            @Override    
-            public void onReceive(Context context, Intent intent) {    
-                if (intent.getAction().equals("LocationResult")) {
-                	LabelTime.setText(intent.getExtras().getString("Time"));
-                	switch(intent.getExtras().getInt("ErrCode")){
-                	case 61:
-                		errString = "成功。通过GPS定位";
-                		break;
-                	case 161:
-                		errString = "成功。通过基站或WIFI定位";
-                		break;
-                	case 63:
-                		errString = "定准失败。网络异常";
-                	case 68:
-                		errString = "成功。通过缓存获取定位信息";
-                		break;
-                	default:
-                		errString = "失败。错误代码" + intent.getExtras().getInt("ErrCode");
-                		break;
-                	}
-                	LabelErrcode.setText(errString);
-                	double lng = intent.getExtras().getDouble("Longitude");
-                	double lat = intent.getExtras().getDouble("Latitude");
-                	LabelLatLng.setText( lng + "," + lat );
-                	double gauss[] = CoordsTrans.ToGaussProj(lng, lat);
-                	LabelGauss.setText(Math.round(gauss[0]) + "," + Math.round(gauss[1]));
-                	LabelRadius.setText(Float.toString(intent.getExtras().getFloat("Radius")));
-                	LocResult.setText(intent.getExtras().getString("LocationResult"));
 
-                }   
-            }    
-        };    
-		mLocalBroadcastManager.registerReceiver(mReceiver, filter);
 	}
 	
 	@Override
@@ -240,7 +245,7 @@ public class LocationActivity extends AppCompatActivity {
 	protected void onStop() {
 		// TODO Auto-generated method stub
 		super.onStop();
-		mLocalBroadcastManager.unregisterReceiver(mReceiver);  
+
 	}
 
 	private void InitLocation(){
@@ -273,5 +278,191 @@ public class LocationActivity extends AppCompatActivity {
 		
 		String appUUID = sharedPref.getString("app_uuid","");
 		YunBaManager.subscribe(getApplicationContext(), new String[]{"all", appUUID}, listener);
+	}
+
+	private final BDAbstractLocationListener mListener = new BDAbstractLocationListener() {
+
+		/**
+		 * 定位请求回调函数
+		 * @param location 定位结果
+		 */
+		@Override
+		public void onReceiveLocation(BDLocation location) {
+
+			// TODO Auto-generated method stub
+			if (null != location && location.getLocType() != BDLocation.TypeServerError) {
+				int tag = 1;
+				StringBuffer sb = new StringBuffer(256);
+				sb.append("\nlocType description : ");// *****对应的定位类型说明*****
+				sb.append(location.getLocTypeDescription());
+				sb.append("\naddr : ");// 地址信息
+				sb.append(location.getAddrStr());
+				sb.append("\nStreetNumber : ");// 获取街道号码
+				sb.append(location.getStreetNumber());
+				sb.append("\nUserIndoorState: ");// *****返回用户室内外判断结果*****
+				sb.append(location.getUserIndoorState());
+				sb.append("\nDirection(not all devices have value): ");
+				sb.append(location.getDirection());// 方向
+				sb.append("\nlocationdescribe: ");
+				sb.append(location.getLocationDescribe());// 位置语义化信息
+				sb.append("\nPoi: ");// POI信息
+				if (location.getPoiList() != null && !location.getPoiList().isEmpty()) {
+					for (int i = 0; i < location.getPoiList().size(); i++) {
+						Poi poi = (Poi) location.getPoiList().get(i);
+						sb.append("poiName:");
+						sb.append(poi.getName() + ", ");
+						sb.append("poiTag:");
+						sb.append(poi.getTags() + "\n");
+					}
+				}
+				if (location.getPoiRegion() != null) {
+					sb.append("PoiRegion: ");// 返回定位位置相对poi的位置关系，仅在开发者设置需要POI信息时才会返回，在网络不通或无法获取时有可能返回null
+					PoiRegion poiRegion = location.getPoiRegion();
+					sb.append("DerectionDesc:"); // 获取POIREGION的位置关系，ex:"内"
+					sb.append(poiRegion.getDerectionDesc() + "; ");
+					sb.append("Name:"); // 获取POIREGION的名字字符串
+					sb.append(poiRegion.getName() + "; ");
+					sb.append("Tags:"); // 获取POIREGION的类型
+					sb.append(poiRegion.getTags() + "; ");
+					sb.append("\nSDK版本: ");
+				}
+				sb.append(locService.getSDKVersion()); // 获取SDK版本
+				int permission = checkPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION);
+				sb.append("\npermsission: " + permission);
+				if (location.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
+					sb.append("\nspeed : ");
+					sb.append(location.getSpeed());// 速度 单位：km/h
+					sb.append("\nsatellite : ");
+					sb.append(location.getSatelliteNumber());// 卫星数目
+					sb.append("\nheight : ");
+					sb.append(location.getAltitude());// 海拔高度 单位：米
+					sb.append("\ngps status : ");
+					sb.append(location.getGpsAccuracyStatus());// *****gps质量判断*****
+					sb.append("\ndescribe : ");
+					sb.append("gps定位成功");
+				} else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
+					// 运营商信息
+					if (location.hasAltitude()) {// *****如果有海拔高度*****
+						sb.append("\nheight : ");
+						sb.append(location.getAltitude());// 单位：米
+					}
+					sb.append("\noperationers : ");// 运营商信息
+					sb.append(location.getOperators());
+					sb.append("\ndescribe : ");
+					sb.append("网络定位成功");
+				} else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
+					sb.append("\ndescribe : ");
+					sb.append("离线定位成功，离线定位结果也是有效的");
+				} else if (location.getLocType() == BDLocation.TypeServerError) {
+					sb.append("\ndescribe : ");
+					sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
+				} else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+					sb.append("\ndescribe : ");
+					sb.append("网络不同导致定位失败，请检查网络是否通畅");
+				} else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+					sb.append("\ndescribe : ");
+					sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+				}
+				System.out.println(sb.toString());
+
+				LabelTime.setText(location.getTime());
+				switch(location.getLocType()){
+					case 61:
+						errString = "成功。通过GPS定位";
+						break;
+					case 161:
+						errString = "成功。通过基站或WIFI定位";
+						break;
+					case 63:
+						errString = "定准失败。网络异常";
+					case 68:
+						errString = "成功。通过缓存获取定位信息";
+						break;
+					default:
+						errString = "失败。错误代码" + location.getLocType();
+						break;
+				}
+				LabelErrcode.setText(errString);
+				double lng = location.getLatitude();
+				double lat = location.getLongitude();
+				LabelLatLng.setText( lng + "," + lat );
+				double gauss[] = CoordsTrans.ToGaussProj(lng, lat);
+				LabelGauss.setText(Math.round(gauss[0]) + "," + Math.round(gauss[1]));
+				LabelRadius.setText(Float.toString(location.getRadius()));
+				LocResult.setText(sb.toString());
+			}
+		}
+
+		@Override
+		public void onConnectHotSpotMessage(String s, int i) {
+			super.onConnectHotSpotMessage(s, i);
+		}
+
+		/**
+		 * 回调定位诊断信息，开发者可以根据相关信息解决定位遇到的一些问题
+		 * @param locType 当前定位类型
+		 * @param diagnosticType 诊断类型（1~9）
+		 * @param diagnosticMessage 具体的诊断信息释义
+		 */
+		@Override
+		public void onLocDiagnosticMessage(int locType, int diagnosticType, String diagnosticMessage) {
+			super.onLocDiagnosticMessage(locType, diagnosticType, diagnosticMessage);
+			int tag = 2;
+			StringBuffer sb = new StringBuffer(256);
+			sb.append("诊断结果: ");
+			if (locType == BDLocation.TypeNetWorkLocation) {
+				if (diagnosticType == 1) {
+					sb.append("网络定位成功，没有开启GPS，建议打开GPS会更好");
+					sb.append("\n" + diagnosticMessage);
+				} else if (diagnosticType == 2) {
+					sb.append("网络定位成功，没有开启Wi-Fi，建议打开Wi-Fi会更好");
+					sb.append("\n" + diagnosticMessage);
+				}
+			} else if (locType == BDLocation.TypeOffLineLocationFail) {
+				if (diagnosticType == 3) {
+					sb.append("定位失败，请您检查您的网络状态");
+					sb.append("\n" + diagnosticMessage);
+				}
+			} else if (locType == BDLocation.TypeCriteriaException) {
+				if (diagnosticType == 4) {
+					sb.append("定位失败，无法获取任何有效定位依据");
+					sb.append("\n" + diagnosticMessage);
+				} else if (diagnosticType == 5) {
+					sb.append("定位失败，无法获取有效定位依据，请检查运营商网络或者Wi-Fi网络是否正常开启，尝试重新请求定位");
+					sb.append(diagnosticMessage);
+				} else if (diagnosticType == 6) {
+					sb.append("定位失败，无法获取有效定位依据，请尝试插入一张sim卡或打开Wi-Fi重试");
+					sb.append("\n" + diagnosticMessage);
+				} else if (diagnosticType == 7) {
+					sb.append("定位失败，飞行模式下无法获取有效定位依据，请关闭飞行模式重试");
+					sb.append("\n" + diagnosticMessage);
+				} else if (diagnosticType == 9) {
+					sb.append("定位失败，无法获取任何有效定位依据");
+					sb.append("\n" + diagnosticMessage);
+				}
+			} else if (locType == BDLocation.TypeServerError) {
+				if (diagnosticType == 8) {
+					sb.append("定位失败，请确认您定位的开关打开状态，是否赋予APP定位权限");
+					sb.append("\n" + diagnosticMessage);
+				}
+			}
+			System.out.println(sb.toString());
+		}
+	};
+
+	public static int checkPermission(Context context, String permission) {
+
+		boolean allowedByPermission = true;
+		try {
+			int result = context.checkPermission(permission, android.os.Process.myPid(), android.os.Process.myUid());
+			allowedByPermission = (result == PackageManager.PERMISSION_GRANTED);
+		} catch (Exception e) {
+			allowedByPermission = true;
+		}
+		if (!allowedByPermission) {
+			return 0;
+		} else {
+			return 1;
+		}
 	}
 }
---
+++ android/app/src/main/java/com/wlwdw/gps1s/MainActivity.java
@@ -0,0 +1,310 @@
+package com.wlwdw.gps1s;
+
+import com.wlwdw.gps1s.service.LocService;
+import com.wlwdw.gps1s.service.Utils;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.TextUtils;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.baidu.location.LocationClient;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainActivity extends Activity {
+    private final int SDK_PERMISSION_REQUEST = 127;
+    private ListView FunctionList;
+    private String permissionInfo;
+
+    private final int DIALOG_KEY_BACK = 1;
+    private final int DIALOG_PERMISSION = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        //创建弹窗显示隐私政策
+        if (!Utils.contains(this, Utils.SP_PRIVACY_DIALOG)) {
+            createPrivacyDialog();
+        } else {
+            boolean status = Utils.getString(MainActivity.this, Utils.SP_PRIVACY_STATUS).equals("1");
+            initSDK(status);
+        }
+
+        FunctionList = (ListView) findViewById(R.id.functionList);
+        FunctionList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, getData()));
+        // after andrioid m,must request Permiision on runtime
+        IntentFilter filter = new IntentFilter();
+        // 点击home键广播，由系统发出
+        filter.addAction(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
+        registerReceiver(mHomeAndLockReceiver, filter);
+    }
+
+    private void initSDK(boolean status) {
+        LocationClient.setAgreePrivacy(status);
+        ((LocationApplication)getApplication()).locService = new LocService(getApplicationContext());
+
+        onUsePermission();
+        getPersimmions();
+    }
+
+    private void createPrivacyDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        String notifyString = "为进一步加强对最终用户个人信息的安全保护措施, 请仔细阅读如下隐私政策并确认是否同意：\n《服务隐私政策》";
+        SpannableStringBuilder spannableString = new SpannableStringBuilder(notifyString);
+        Pattern pattern = Pattern.compile("《服务隐私政策》");
+        Matcher matcher = pattern.matcher(spannableString);
+        while (matcher.find()) {
+            setClickableSpan(spannableString, matcher);
+        }
+
+        View view = View.inflate(this, R.layout.notify_privacy_text, null);
+        TextView notifyText = (TextView) view.findViewById(R.id.notify_text);
+        notifyText.setText(spannableString);
+        notifyText.setMovementMethod(LinkMovementMethod.getInstance());
+
+        builder.setView(view);
+        builder.setPositiveButton("同意", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                initSDK(true);
+                Utils.putString(MainActivity.this, Utils.SP_PRIVACY_DIALOG, Utils.SP_PRIVACY_DIALOG);
+                Utils.putString(MainActivity.this, Utils.SP_PRIVACY_STATUS, "1");
+            }
+        });
+
+        builder.setNegativeButton("不同意", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                initSDK(false);
+                Utils.putString(MainActivity.this, Utils.SP_PRIVACY_STATUS, "0");
+            }
+        });
+
+
+        AlertDialog dialog = builder.create();
+        dialog.setCancelable(false);
+        dialog.show();
+        Button positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+        Button negativeButton = dialog.getButton(AlertDialog.BUTTON_NEGATIVE);
+        LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) positiveButton.getLayoutParams();
+        layoutParams.weight = 10;
+        positiveButton.setLayoutParams(layoutParams);
+        negativeButton.setLayoutParams(layoutParams);
+    }
+
+    private void setClickableSpan(SpannableStringBuilder span, Matcher matcher) {
+        int start = matcher.start();
+        int end = matcher.end();
+
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                final Uri uri = Uri.parse("https://lbsyun.baidu.com/index.php?title=openprivacy");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+
+            @Override
+            public void updateDrawState(TextPaint textPaint) {
+                textPaint.setUnderlineText(false);
+            }
+        };
+
+        span.setSpan(new ForegroundColorSpan(Color.CYAN), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        span.setSpan(clickableSpan, start, end, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+    }
+
+    @TargetApi(23)
+    private void getPersimmions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ArrayList<String> permissions = new ArrayList<String>();
+            /***
+             * 定位权限为必须权限，用户如果禁止，则每次进入都会申请
+             */
+            // 定位精确位置
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+            }
+            if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+            }
+            /*
+             * 读写权限和电话状态权限非必要权限(建议授予)只会申请一次，用户同意或者禁止，只会弹一次
+             */
+            // 读写权限
+            if (addPermission(permissions, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                permissionInfo += "Manifest.permission.WRITE_EXTERNAL_STORAGE Deny \n";
+            }
+            if (permissions.size() > 0) {
+                requestPermissions(permissions.toArray(new String[permissions.size()]), SDK_PERMISSION_REQUEST);
+            }
+        }
+    }
+
+    @TargetApi(23)
+    private boolean addPermission(ArrayList<String> permissionsList, String permission) {
+        // 如果应用没有获得对应权限,则添加到列表中,准备批量申请
+        if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+            if (shouldShowRequestPermissionRationale(permission)) {
+                return true;
+            } else {
+                permissionsList.add(permission);
+                return false;
+            }
+        } else {
+            return true;
+        }
+    }
+
+    @TargetApi(23)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        // TODO Auto-generated method stub
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+        FunctionList.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                // TODO Auto-generated method stub
+                Class<?> TargetClass = null;
+                switch (arg2) {
+                    case 0:
+                        TargetClass = LocationActivity.class;
+                        break;
+                    default:
+                        break;
+                }
+                if (TargetClass != null) {
+                    Intent intent = new Intent(MainActivity.this, TargetClass);
+                    intent.putExtra("from", 0);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+    private List<String> getData() {
+        List<String> data = new ArrayList<String>();
+        data.add("进入定位功能");
+        return data;
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (!Utils.contains(this, Utils.SP_KEY_BACK_RETURN)) {
+                Utils.putString(this, Utils.SP_KEY_BACK_RETURN, Utils.SP_KEY_BACK_RETURN);
+                showMissingPermissionDialog("提示", getString(R.string.action_background), DIALOG_KEY_BACK);
+                return false;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    /**
+     * 显示提示信息
+     */
+    private void showMissingPermissionDialog(String title, String message, final int type) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title);
+        builder.setMessage(message);
+
+        builder.setPositiveButton("确定",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (type == DIALOG_KEY_BACK) {
+                            Intent intent = new Intent(Intent.ACTION_MAIN);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            intent.addCategory(Intent.CATEGORY_HOME);
+                            startActivity(intent);
+                        } else {
+                            dialog.dismiss();
+                        }
+
+                    }
+                });
+
+        builder.setCancelable(false);
+
+        builder.show();
+    }
+
+    private void onUsePermission() {
+        if (!Utils.contains(this, Utils.SP_PERMISSION_DIALOG)) {
+            Utils.putString(this, Utils.SP_PERMISSION_DIALOG, Utils.SP_PERMISSION_DIALOG);
+            showMissingPermissionDialog("定位SDK DEMO在使用时需要申请以下权限：", "1、定位权限，用于定位功能测试。\n2、读写权限，用于写入离线定位数据。\n", DIALOG_PERMISSION);
+        }
+    }
+
+    /**
+     * 监听是否点击了home键将客户端推到后台
+     */
+    private BroadcastReceiver mHomeAndLockReceiver = new BroadcastReceiver() {
+        String SYSTEM_REASON = "reason";
+        String SYSTEM_HOME_KEY = "homekey";
+        String SYSTEM_HOME_RECENT = "recentapps";
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(Intent.ACTION_CLOSE_SYSTEM_DIALOGS)) {
+                String reason = intent.getStringExtra(SYSTEM_REASON);
+                if (TextUtils.equals(reason, SYSTEM_HOME_KEY) || TextUtils.equals(reason, SYSTEM_HOME_RECENT)) {
+                    if (!Utils.contains(MainActivity.this, Utils.SP_HOME_BACK_RETURN)) {
+                        Utils.putString(MainActivity.this, Utils.SP_HOME_BACK_RETURN, Utils.SP_HOME_BACK_RETURN);
+                        Toast.makeText(context, context.getString(R.string.action_background), Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mHomeAndLockReceiver);
+    }
+}
