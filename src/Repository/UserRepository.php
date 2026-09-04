779d421b077a1bdeca7d430e9d45121d3c857d97
Emfox Zhou <emfoxzhou@gmail.com>
2026-09-04 13:08:57 +0800

    Remove leftover dead templates, access rules and stale repo methods
    - Delete templates/FOSUserBundle/ (layout leftover from the long-gone
      FOSUserBundle integration).
    - security.yaml: drop the ^/register and ^/resetting access_control
      rules, no such routes exist anymore (FOSUser era remnants).
    - UserRepository: remove the unused add()/remove() scaffold methods and
      their Doctrine\ORM\ORMException / OptimisticLockException references
      (ORMException is deprecated since ORM 2.15 and removed in ORM 3).
      upgradePassword() is kept.
    
    Behaviour unchanged.

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -4,8 +4,6 @@ namespace App\Repository;
 
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\ORM\OptimisticLockException;
-use Doctrine\ORM\ORMException;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
@@ -26,29 +24,7 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         parent::__construct($registry, User::class);
     }
 
-    /**
-     * @throws ORMException
-     * @throws OptimisticLockException
-     */
-    public function add(User $entity, bool $flush = true): void
-    {
-        $this->_em->persist($entity);
-        if ($flush) {
-            $this->_em->flush();
-        }
-    }
 
-    /**
-     * @throws ORMException
-     * @throws OptimisticLockException
-     */
-    public function remove(User $entity, bool $flush = true): void
-    {
-        $this->_em->remove($entity);
-        if ($flush) {
-            $this->_em->flush();
-        }
-    }
 
     /**
      * Used to upgrade (rehash) the user's password automatically over time.
